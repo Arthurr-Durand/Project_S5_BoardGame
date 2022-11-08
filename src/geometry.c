d5cc23ba521998262389ec8989327c98e7e477ef
DURAND Arthur <Arthur.Durand@bordeaux-inp.fr>
2022-11-08 16:05:00 +0100

    Code geometry (compile)

---
+++ src/geometry.c
@@ -0,0 +1,47 @@
+#include "geometry.h"
+
+
+const char* place_to_string(enum color_t c,enum sort_t s)
+{
+    //à remplir 
+        const char* t="bla";
+    return t;
+}
+
+const char* dir_to_string(enum dir_t d)
+{
+    // à remplir 
+    const char* t="bla";
+    return t;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(int agrc, char* argv[])
+{
+    return 0; 
+}
