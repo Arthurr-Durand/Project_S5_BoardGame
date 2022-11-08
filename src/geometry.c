4d07c3fe26e6faeecafe8a1b03e0961fe3e2670b
DURAND Arthur <Arthur.Durand@bordeaux-inp.fr>
2022-11-08 17:16:59 +0100

    premières fonction c'est partie!

--- src/geometry.c
+++ src/geometry.c
@@ -1,5 +1,6 @@
 #include "geometry.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 
 const char* place_to_string(enum color_t c,enum sort_t s)
 {
@@ -10,9 +11,42 @@ const char* place_to_string(enum color_t c,enum sort_t s)
 
 const char* dir_to_string(enum dir_t d)
 {
-    // à remplir 
-    const char* t="bla";
-    return t;
+    const char* D="No_dir";
+    const char* E="East";
+    const char* NE="Neast";
+    const char* N="North";
+    const char* NW="Nwest";
+    const char* W="West";
+    const char* SW="Swest";
+    const char* S="South";
+    const char* SE="Seast";
+    switch (d)
+    {
+    case 0:
+        return D;
+    case 1:
+        return E;
+    case 2:
+        return NE;
+    case 3:
+        return N;
+    case 4:
+        return NW;
+    case -1:
+        return W;
+    case -2:
+        return SW;
+    case -3:
+        return S;
+    case -4: 
+        return SE;
+
+    
+    default:
+        return "pas compris";
+    }
+
+    
 }
 
 
@@ -43,5 +77,6 @@ const char* dir_to_string(enum dir_t d)
 
 int main(int agrc, char* argv[])
 {
+    printf("%s\n",dir_to_string(atoi(argv[1])));
     return 0; 
 }
---
+++ src/world.c
@@ -0,0 +1,19 @@
+#include "world.h"
+#include <stdio.h>
+
+void world_set(struct world_t* b, unsigned int idx, enum color_t c)
+{
+    printf("aaa\n");
+}
+
+
+
+void world_set_sort(struct world_t* b, unsigned int idx, enum sort_t c)
+{
+    printf("aaa\n");
+}
+
+int main(int argc,char* argv[])
+{
+    return 0;
+}
