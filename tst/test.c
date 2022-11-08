de800e6fc7c66c5092f520e2f2107d34bb72e8ad
HAMOUCHE Luxel <Luxel.Hamouche@bordeaux-inp.fr>
2022-11-08 17:33:53 +0100

    nouveau test

--- tst/test.c
+++ tst/test.c
@@ -2,24 +2,28 @@
 
 #include "../src/geometry.h"
 
+/*
 void test_place_to_string()
 {
     printf("test_place_to_string :\n");
     printf("%d, %d > %s\n", 2, 1, place_to_string(2, 1));
     printf("%d, %d > %s\n", 0, 3, place_to_string(0, 3));
 }
+*/
 
-void dir_to_string()
+void test_dir_to_string()
 {
-    printf("test_place_to_string :\n");
-    printf("%d, %d > %s\n", -2, 1, dir_to_string(-2, 1));
-    printf("%d, %d > %s\n", 0, 3, dir_to_string(0, 3));
-    printf("%d, %d > %s\n", -5, 12, dir_to_string(-5, 12));
+    printf("test_dir_to_string :\n");
+    printf("%d > %s\n", -2, dir_to_string(-2));
+    printf("%d > %s\n", 3, dir_to_string(3));
+    printf("%d > %s\n", -5, dir_to_string(-5));
+    printf("%d > %s\n", 12, dir_to_string(12));
 }
 
 int main()
 {
     printf("test.c :\n");
-    test_place_to_string();
+    //test_place_to_string();
+    test_dir_to_string();
     return 0;
 }
