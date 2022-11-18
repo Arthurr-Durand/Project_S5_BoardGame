7d7724211f2cf28f32e991327b4aa593ad9f49c0
DURAND Arthur <Arthur.Durand@bordeaux-inp.fr>
2022-11-18 15:49:18 +0100

    Normalement ça marche! (test neighbors)

--- src/neighbors.c
+++ src/neighbors.c
@@ -20,7 +20,7 @@ unsigned int get_neighbor(unsigned int idx, enum dir_t d) // Fonction faite pour
     switch (idx) { // les coins 
         case 0:
             if (d == NWEST)
-                return (HEIGHT)*WIDTH;
+                return (HEIGHT)*WIDTH-1;
             break;
         case WIDTH:
             if (d == NEAST)
@@ -40,11 +40,11 @@ unsigned int get_neighbor(unsigned int idx, enum dir_t d) // Fonction faite pour
         case NEAST:
             if (idx < WIDTH) //si bord supp  
                 return idx+1+(HEIGHT-1)*WIDTH; //pour un rectangle retourner UNIT_MAX
-            break;
+            return idx-(WIDTH+d-3);
         case NORTH:
             if (idx < WIDTH)
                 return idx+(HEIGHT-1)*WIDTH;
-            break;
+            return idx-(WIDTH+d-3);
         case NWEST:
             if (idx < WIDTH)
                 return idx-1+(HEIGHT-1)*WIDTH;
@@ -52,11 +52,11 @@ unsigned int get_neighbor(unsigned int idx, enum dir_t d) // Fonction faite pour
         case SEAST:
             if (idx > (HEIGHT*(WIDTH-1))) //si bord inf
                 return idx+1-(HEIGHT-1)*WIDTH;
-            break;
+            return idx+(WIDTH-d-3);
         case SOUTH:
             if (idx > (HEIGHT*(WIDTH-1)))
                 return idx-(HEIGHT-1)*WIDTH;
-            break;
+            return idx+(WIDTH-d-3);
         case SWEST:
             if (idx > (HEIGHT*(WIDTH-1)))
                 return idx-1-(HEIGHT-1)*WIDTH;
@@ -64,7 +64,7 @@ unsigned int get_neighbor(unsigned int idx, enum dir_t d) // Fonction faite pour
         case EAST:
             if (idx%WIDTH == 9) //si bord droit 
                 return idx-WIDTH+1;
-            break;
+            return idx+d;
         case WEST:
             if (idx%WIDTH == 0) //si bord gauche 
                 return idx+WIDTH-1;
@@ -81,16 +81,17 @@ struct neighbors_t get_neighbors(unsigned int idx)
 {
     struct neighbors_t voisins;
     int p=0;
-    for(enum dir_t i=SEAST;i<=MAX_DIR/2;i++){
-        if (get_neighbor(idx,i)!=UINT_MAX){
-            voisins.n[p].i = get_neighbor(idx,i);
-            voisins.n[p].d = i;
+    for(enum dir_t d=SEAST;d<=(MAX_DIR/2);d++){
+        if (get_neighbor(idx,d)!=UINT_MAX){
+            printf("%d\n",d);
+            voisins.n[p].i = get_neighbor(idx,d);
+            voisins.n[p].d = d;
             printf("idx:%d, dir:%s\n",voisins.n[p].i,dir_to_string(voisins.n[p].d));
             p++;
         }
     }
     voisins.n[p].i=UINT_MAX;
-    voisins.n[p].i=NO_DIR;
+    voisins.n[p].d=NO_DIR;
     printf("idx:%d, dir:%s\n\n",voisins.n[p].i,dir_to_string(voisins.n[p].d));
     return voisins;
 }
