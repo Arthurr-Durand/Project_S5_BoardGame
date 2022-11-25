730adc74e0e1b6d715423bfb78122e57bff5c5f6
HAMOUCHE Luxel <Luxel.Hamouche@bordeaux-inp.fr>
2022-11-25 16:32:48 +0100

    New world struct

--- src/world.c
+++ src/world.c
@@ -4,17 +4,11 @@
 #include "geometry.h"
 #include "world.h"
 
-/** Struct representing a place in a world*/
-struct places_t {
-    unsigned int index; // The index of the place
-    enum color_t color; // The color of the place
-    enum sort_t sort; // The sort of the place
-};
-
 /** Abstract struct representing the possible places in the world
     These places are indexed from 0 to WORLD_SIZE-1 */
 struct world_t {
-    struct places_t p[WORLD_SIZE]; // The list of all places in the world
+    enum color_t colors[WORLD_SIZE];
+    enum sort_t sorts[WORLD_SIZE];
 };
 
 struct world_t world;
@@ -23,9 +17,8 @@ struct world_t world;
     Always returns the same pointer */
 struct world_t* world_init() {
     for (int i = 0; i < WORLD_SIZE; ++i) {
-        world.p[i].index = i;
-        world.p[i].color = NO_COLOR;
-        world.p[i].sort = NO_SORT;
+        world.colors[i] = NO_COLOR;
+        world.sorts[i] = NO_SORT;
     }
     return &world;
 };
@@ -33,23 +26,23 @@ struct world_t* world_init() {
 /** Return the color at a place */
 enum color_t world_get(const struct world_t* b, unsigned int idx)
 {
-    return b->p[idx].color;
+    return b->colors[idx];
 }
 
 /** Sets the color at a place */
 void world_set(struct world_t* b, unsigned int idx, enum color_t c)
 {
-    b->p[idx].color = c;
+    b->colors[idx] = c;
 }
 
 /** Return the sort at a place */
 enum sort_t world_get_sort(const struct world_t* b, unsigned int idx)
 {
-    return b->p[idx].sort;
+    return b->sorts[idx];
 }
 
 /** Sets the sort at a place */
 void world_set_sort(struct world_t* b, unsigned int idx, enum sort_t c)
 {
-    b->p[idx].sort = c;
+    b->sorts[idx] = c;
 }
