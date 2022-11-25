de5c4c683c1647f06aa824f7dd2b1bd8428ebd59
DURAND Arthur <Arthur.Durand@bordeaux-inp.fr>
2022-11-25 16:02:26 +0100

    Avec le Point virgule

--- src/pawn.h
+++ src/pawn.h
@@ -15,4 +15,4 @@ struct pawns_t* pawns_init(int max_dep, int idx);
 struct sets_t* pawns_all_moves(struct pawns_t* piece, struct world_t* world);
 
 /** Move a Pawn */
-void pawns_moves(struct pawns_t* piece,int idx)
+void pawns_moves(struct pawns_t* piece,int idx);
