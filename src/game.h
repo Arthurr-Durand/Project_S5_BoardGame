6d5b691d6ed9226defe5362ea18b45035d629f08
DURAND Arthur <Arthur.Durand@bordeaux-inp.fr>
2022-12-02 17:42:26 +0100

    Avec le .h c'est mieux :)

--- src/game.h
+++ src/game.h
@@ -12,4 +12,7 @@ void print_game(const struct world_t* world);
 /** Check the winning conditions */
 int game_winning_cond(struct players_t* player, struct sets_t set[], struct pawns_t* pawn,int nb_players);
 
+/** Check the complex winning conditions */
+int game_complex_winning_cond(struct players_t* player, struct sets_t set[],int nb_players);
+
 #endif // __GAME_H__
