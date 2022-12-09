d82fd2e0105d6a7d7deeead4ed426468b59a86e2
DURAND Arthur <Arthur.Durand@bordeaux-inp.fr>
2022-12-09 17:04:34 +0100

    mise en place du plateau avec plusieurs pieces

--- src/players.h
+++ src/players.h
@@ -30,7 +30,7 @@ enum color_t players_get_color(const struct players_t* players);
 void players_add_pawn(struct players_t* player, int max_dep, enum sort_t type, int position);
 
 /** Sets all world sort in inital sets to pawns */
-void players_set_initial_pawns(struct world_t* world, struct players_t players[], const int nb_players, const struct sets_t sets[], int max_dep, enum sort_t type);
+void players_set_initial_pawns(struct world_t* world, struct players_t players[], const int nb_players, const struct sets_t sets[], int max_dep, enum sort_t type, int foramt, int forame);
 
 /** Return a random player's pawn */
 struct pawns_t* players_get_random_pawn(struct players_t* player);
